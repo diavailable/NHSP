926d8c876574dda7b4d5a7f17c0f2a0926e273f6
diavailable <156864971+diavailable@users.noreply.github.com>
2024-07-27 09:15:55 +0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using NHSP.Models;
+using NHSP.Formula;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Threading;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Drawing;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace NHSP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
