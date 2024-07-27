83893d127d03eb24f0183a300e6f16ee043e609a
diavailable <156864971+diavailable@users.noreply.github.com>
2024-07-27 10:00:44 +0800

    fileupdate

---
+++ Controllers/NewHireController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using NHSP.Models;
+using NHSP.Formula;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Threading;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Drawing;
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace NHSP.Controllers
+{
+    public class NewHireController : Controller
+    {
+        private readonly ILogger<NewHireController> _logger;
+
+        public NewHireController(ILogger<NewHireController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Controllers/PayrollController.cs
+++ Controllers/PayrollController.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NHSP.Models;
 using NHSP.Formula;
 using System.Diagnostics;
@@ -14,36 +14,17 @@ using System.Xml.Linq;
 using System.Threading;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 using System.Drawing;
-using System.Diagnostics;
 using System.ComponentModel;
 using System.Linq;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 
 namespace NHSP.Controllers
 {
-    public class HomeController : Controller
+    public class PayrollController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
